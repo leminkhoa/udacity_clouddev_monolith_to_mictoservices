d6197df594d9799a030fb3007689b38f796b957f
Khoa, Le <leminkhoa@gmail.com>
2023-05-03 13:55:23 +0700

    Revert to standard repo structure

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ab4d631771818400cae1bf1bcfbc235d-234345487.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
